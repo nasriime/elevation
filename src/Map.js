9792a900dc423593ebc6600f6581b8e542eea267
nasriime <mnasr4040@gmail.com>
2021-12-20 20:14:17 +0200

    add marker

--- src/Map.js
+++ src/Map.js
@@ -1,20 +1,43 @@
+import React, {useState} from 'react';
+import L from 'leaflet';
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
 import "leaflet/dist/leaflet.css";
+import icon from 'leaflet/dist/images/marker-icon.png';
+import iconShadow from 'leaflet/dist/images/marker-shadow.png';
 import "./Map.css";
 
 function Map(){
+    const [markers, setMarkers] = useState([[51.505, -0.09]])
+    
+    const addMarker = (e) => {
+        markers.pop();
+        markers.push(e.latlng);
+        setMarkers(markers)
+    }
+
+    let DefaultIcon = L.icon({
+        iconUrl: icon,
+        shadowUrl: iconShadow
+    });
+    L.Marker.prototype.options.icon = DefaultIcon;
+
     return(
         <div>
-           <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
-            <TileLayer
-                attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-            />
-            <Marker position={[51.505, -0.09]}>
-                <Popup>
-                A pretty CSS3 popup. <br /> Easily customizable.
-                </Popup>
-            </Marker>
+           <MapContainer 
+                center={[51.505, -0.09]}       
+                onClick={addMarker}
+                zoom={13} >
+                <TileLayer
+                    attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                />
+                {markers.map((position, idx) => 
+                    <Marker key={`marker-${idx}`} position={position}>
+                        <Popup>
+                            A pretty CSS3 popup. <br /> Easily customizable.
+                        </Popup>
+                    </Marker>
+                )}
             </MapContainer>
         </div>
     )
